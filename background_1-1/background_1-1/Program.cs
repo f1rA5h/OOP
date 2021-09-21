8e3143c48042843e65a4f9ad0bfe5298eb7f5eae
f1rA5h <shtey.alexey@gmail.com>
2021-09-21 23:15:47 +0300

    some stuff

--- background_1-1/background_1-1/Program.cs
+++ background_1-1/background_1-1/Program.cs
@@ -4,19 +4,22 @@ namespace background_1_1
 {
     class Program
     {
-        static double CalcFunc10(int a, int b, int c)
-        {
-
-            return (double)(a + c) / (b + a) +
-                Math.Pow(
-                    (2 * Math.Sin(b) + a),
-                    (1.0 / 3.0)) +
-                Math.Pow(c, 2);
-        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(CalcFunc10(1, 2, 3));
+            int a = int.Parse(Console.ReadLine());  // test:        1
+            int b = int.Parse(Console.ReadLine());  // test:        2
+            int c = int.Parse(Console.ReadLine());  // test:        3
+
+            double result;                          // result: 11.574
+
+            result = (double)(a + c) / (b + a) +
+                Math.Pow(
+                    (Math.Sin(b) + a),
+                    (1.0 / 3.0)) +
+                Math.Pow(c, 2);
+            
+            Console.WriteLine($"{result:f3}");
             Console.ReadKey();
         }
         
