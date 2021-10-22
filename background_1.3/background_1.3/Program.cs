f0d989f79fc6c7cedde5686ffad9053dc644a773
f1rA5h <shtey.alexey@gmail.com>
2021-10-22 11:31:50 +0300

    some stuff

--- background_1.3/background_1.3/Program.cs
+++ background_1.3/background_1.3/Program.cs
@@ -4,16 +4,15 @@ namespace background_1._3
 {
     class Program
     {
-    static double maxSum(int n)
+        static double maxSum(int n)
         {
             double a1, a2;
             double tmp;
-
-
-            a1 = Convert.ToDouble(Console.ReadLine()); 
+                
+            a1 = Convert.ToDouble(Console.ReadLine()); //  начальные значения
             a2 = Convert.ToDouble(Console.ReadLine()); 
 
-            tmp = a1 + a2; 
+            tmp = a1 + a2; // 
             for (int i = 2; i < n; i++) 
             {
                 a1 = a2; 
@@ -30,9 +29,14 @@ namespace background_1._3
 
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
-            double maxSumResult = maxSum(n);
-            Console.WriteLine($"{maxSumResult:f3}");
+            int n = Convert.ToInt32(Console.ReadLine()); // кол-во чисел в последовательности
+            if (n < 2) 
+                Console.WriteLine("У одного числа не будет соседних");
+            else
+            {
+                double maxSumResult = maxSum(n);
+                Console.WriteLine($"{maxSumResult:f3}");
+            }
         }
     }
 }
