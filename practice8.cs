bc9edf4b693afd5219971141549fab1a16627b04
f1rA5h <shtey.alexey@gmail.com>
2021-12-03 11:58:10 +0300

    practice8 added

---
+++ practice8.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace practice7
+{
+    static class Program
+    {
+        static int F(int n)
+        {
+            if(n <= 0) return n;
+            else if(n > 0 && n % 5 == 0) return n + F(n / 5 - 1);
+            else if(n > 0 && n % 5 != 0) return n + F(n + 6);
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int result = F(n);
+            Console.WriteLine(result);
+        }
+    }
+}
