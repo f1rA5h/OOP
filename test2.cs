ded2b494ad204280d88d56e9a569c3eaf6b8e254
f1rA5h <shtey.alexey@gmail.com>
2021-12-23 00:01:51 +0300

    sbyte test

---
+++ test2.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace test
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte a = 125;
+            sbyte b = 14;
+            Console.Write((sbyte)(a + b));
+        }
+    }
+}
